9e9292ed793fa46998a961af8b5d0eff41253b0b
achetwa <anujchetwani@gmail.com>
2023-10-13 23:33:47 -0400

    feat: generate a CSV

--- code/extract.py
+++ code/extract.py
@@ -2,6 +2,8 @@ import helper
 import logging
 from telegram import InputFile
 from telebot import types
+import csv
+import os
 
 # The main funtion of category.py.
 # User can start to manage their categories after calling it
@@ -12,8 +14,15 @@ def run(message, bot):
     try:
         chat_id = message.chat.id
         user_history = helper.getUserHistory(chat_id)
-        file_path = 'code/sample.csv'
-
+        file_path = 'code/data.csv'
+        rows = [line.split(',') for line in user_history]
+        column_names = ['Date and Time', 'Category', 'Amount']
+        with open(file_path, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(column_names)
+            writer.writerows(rows)
+        
+        
         """
         TODO
         1. Convert user_history to csv
@@ -25,5 +34,6 @@ def run(message, bot):
         # Send the file to the user
         with open(file_path, 'rb') as file:
             bot.send_document(chat_id, document=file)
+        os.remove(file_path)    
     except Exception as e:
         logging.error(str(e))
