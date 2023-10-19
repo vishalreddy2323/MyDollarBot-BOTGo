e625bb400473ad0c95a3c6d30c915730b9bfc1dd
bhaveshittadwar <bitsadwar@gmail.com>
2023-10-19 17:40:02 -0400

    fix: user messages and errors

--- code/receipt.py
+++ code/receipt.py
@@ -15,14 +15,12 @@ api_token = str(configs.get('api_token').data)
 
 bot = telebot.TeleBot(api_token)
 
-
 @bot.message_handler(commands=['receipt'])
 def command_receipt(message, bot):
     chat_id = message.chat.id
     bot.send_message(
         chat_id, 'Please enter the date (YYYY-MM-DD or YYYYMMDD) for which you want to retrieve receipts:')
     bot.register_next_step_handler(message, process_date_input)
-
 
 def process_date_input(message):
     try:
@@ -35,7 +33,7 @@ def process_date_input(message):
             datetime.strptime(user_date, "%Y%m%d")
         except ValueError:
             bot.send_message(
-                chat_id, 'Invalid date format. Please use YYYY-MM-DD format.')
+                chat_id, 'Invalid date format. Please use YYYY-MM-DD or YYYYMMDD format.')
             return
 
         # Search for files in the receipts folder with the dates in their names
@@ -53,7 +51,7 @@ def process_date_input(message):
                     "An error occurred while sending images: " + str(e))
         else:
             bot.send_message(
-                chat_id, 'No receipts found for the specified date.')
+                chat_id, 'No receipts for this date.')
 
     except Exception as e:
         bot.send_message(chat_id, 'An error occurred: ' + str(e))
